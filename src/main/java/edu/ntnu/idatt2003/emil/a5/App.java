1878c67d80a9004c0df8e72c2c03fd62edcc940a
Emil-F <e.fagerjord@gmail.com>
2026-03-06 23:15:31 +0100

    feat: added DeckOfCards class and implemented PlayingCard from the assignment

--- src/main/java/edu/ntnu/idatt2003/emil/a5/App.java
+++ src/main/java/edu/ntnu/idatt2003/emil/a5/App.java
@@ -1,4 +1,15 @@
 package edu.ntnu.idatt2003.emil.a5;
 
+import edu.ntnu.idatt2003.emil.a5.model.DeckOfCards;
+import edu.ntnu.idatt2003.emil.a5.model.PlayingCard;
+
+import java.util.List;
+
 public class App {
+  static void main(String[] args) {
+    DeckOfCards deck = new DeckOfCards();
+    List<PlayingCard> cards = deck.getCards();
+
+    cards.forEach(card -> System.out.println(card.getAsString()));
+  }
 }
--- src/main/java/edu/ntnu/idatt2003/emil/a5/model/DeckOfCards.java
+++ src/main/java/edu/ntnu/idatt2003/emil/a5/model/DeckOfCards.java
@@ -1,4 +1,40 @@
 package edu.ntnu.idatt2003.emil.a5.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DeckOfCards {
+  private final char[] suit = {'S', 'H', 'D', 'C'};
+  private final List<PlayingCard> cards;
+
+  public DeckOfCards() {
+    cards = new ArrayList<>();
+    int face = 1;
+    int index = 0;
+
+    for(int n = 0; n < 52; n++) {
+      if (face > 13) {
+        face = 1;
+        index++;
+      }
+      cards.add(new PlayingCard(suit[index], face));
+      face++;
+    }
+
+    if (cards.size() > 52) {
+      throw new IllegalArgumentException("Deck of Cards can't have more than 52 suits");
+    }
+
+    if (cards.size() < 52) {
+      throw new IllegalArgumentException("Deck of Cards can't have less than 52 suits");
+    }
+  }
+
+  public char[] getSuit() {
+    return suit;
+  }
+
+  public List<PlayingCard> getCards() {
+    return cards;
+  }
 }
