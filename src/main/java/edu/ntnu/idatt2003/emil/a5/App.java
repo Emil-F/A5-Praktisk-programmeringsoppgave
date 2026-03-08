488cda5430a52f5720217665d941ec6a9bff9ede
Emil-F <e.fagerjord@gmail.com>
2026-03-08 01:38:00 +0100

    feat: changed the App to better work with Views and Controller

--- src/main/java/edu/ntnu/idatt2003/emil/a5/App.java
+++ src/main/java/edu/ntnu/idatt2003/emil/a5/App.java
@@ -1,15 +1,12 @@
 package edu.ntnu.idatt2003.emil.a5;
 
-import edu.ntnu.idatt2003.emil.a5.model.DeckOfCards;
-import edu.ntnu.idatt2003.emil.a5.model.Hand;
-import edu.ntnu.idatt2003.emil.a5.view.GameView;
-import edu.ntnu.idatt2003.emil.a5.view.HandView;
+import edu.ntnu.idatt2003.emil.a5.controller.GameController;
+import edu.ntnu.idatt2003.emil.a5.view.MainView;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.util.Objects;
@@ -19,8 +16,9 @@ public class App extends Application {
 
   @Override
   public void start(Stage stage) throws Exception {
-    root = new BorderPane();
-    Scene scene = new Scene(root, 1280, 720);
+    GameController gameController = new GameController();
+    MainView mainView = new MainView(gameController);
+    Scene scene = new Scene(mainView, 1280, 720);
 
     try {
       Image icon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/icon.jpg"), "Icon cannot be null"));
@@ -28,21 +26,10 @@ public class App extends Application {
     } catch (NullPointerException e) {
       System.err.println("Exception message: " + e.getMessage());
     }
+
     stage.setTitle("Card Game");
     stage.setScene(scene);
     stage.show();
-
-    root.getChildren().add(new GameView(this));
-
-    DeckOfCards deck = new DeckOfCards();
-    Hand hand = new Hand();
-    hand.setCards(deck.dealHand(5));
-    hand.getCards().forEach(c -> System.out.println(c.getAsString()));
-    System.out.println(hand.checkHand());
-  }
-
-  public void showHandView() {
-    root.getChildren().setAll(new HandView(this));
   }
 
   static void main(String[] args) {
