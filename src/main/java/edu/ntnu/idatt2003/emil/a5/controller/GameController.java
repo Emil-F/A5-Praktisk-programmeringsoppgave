537905f25f41dceb805eeb6251f092898981e801
Emil-F <e.fagerjord@gmail.com>
2026-03-08 01:38:27 +0100

    feat: added GameController and css files for CommunityCards and MainView

--- src/main/java/edu/ntnu/idatt2003/emil/a5/controller/GameController.java
+++ src/main/java/edu/ntnu/idatt2003/emil/a5/controller/GameController.java
@@ -1,4 +1,13 @@
 package edu.ntnu.idatt2003.emil.a5.controller;
 
-public class AppController {
+import java.util.Arrays;
+
+public class GameController {
+  public GameController() {}
+
+  public String[] getCommunityCards() {
+    String[] communityCards = new String[5];
+    Arrays.fill(communityCards, "H1");
+    return communityCards;
+  }
 }
