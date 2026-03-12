94e4b9316bab37e10ffb559df4082c765d2616b1
Emil-F <e.fagerjord@gmail.com>
2026-03-12 23:42:44 +0100

    feat[PokerGame]: added functionality to deal cards and start the game, as well as creating a separate ObservableList for community cards to make it possible to update the UI automatically

--- src/main/java/edu/ntnu/idatt2003/emil/a5/model/PokerGame.java
+++ src/main/java/edu/ntnu/idatt2003/emil/a5/model/PokerGame.java
@@ -2,18 +2,25 @@ package edu.ntnu.idatt2003.emil.a5.model;
 
 import edu.ntnu.idatt2003.emil.a5.model.users.Bot;
 import edu.ntnu.idatt2003.emil.a5.model.users.Player;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.logging.Logger;
 
 public class PokerGame {
+  private final static Logger LOGGER = Logger.getLogger(PokerGame.class.getName());
   private final DeckOfCards deck;
+  private final List<PlayingCard> communityCards;
   private Player player;
   private final List<Bot> bots;
-  private final List<PlayingCard> communityCards;
   // Todo: implement pot system
   // private int pot;
   private int round;
+
+  private ObservableList<PlayingCard> communityCardsObservable = FXCollections.observableArrayList();
 
   public PokerGame() {
     this.deck = new DeckOfCards();
@@ -27,6 +34,24 @@ public class PokerGame {
     return deck;
   }
 
+  public List<PlayingCard> getCommunityCards() {
+    return communityCards;
+  }
+
+  public String getCommunityCardsString() {
+    StringBuilder communityCardsString = new StringBuilder();
+    communityCardsString.append("Community Cards: [");
+    for (int i = 0; i < this.communityCards.size(); i++) {
+      if (i == this.communityCards.size() - 1) {
+        communityCardsString.append(this.communityCards.get(i).getAsString());
+        continue;
+      }
+      communityCardsString.append(this.communityCards.get(i).getAsString()).append(", ");
+    }
+    communityCardsString.append("]");
+    return communityCardsString.toString();
+  }
+
   public Player getPlayer() {
     return player;
   }
@@ -35,41 +60,61 @@ public class PokerGame {
     return bots;
   }
 
-  public List<PlayingCard> getCommunityCards() {
-    return communityCards;
-  }
-
   public int getRound() {
     return round;
   }
 
+  public ObservableList<PlayingCard> getCommunityCardsObservable() {
+    return communityCardsObservable;
+  }
+
   public void setPlayer(Player player) {
-    this.player = player;
+    this.player = Objects.requireNonNull(player, "player is null");
   }
 
   public void populateBots(int botCount) {
+    if (botCount > 22) {
+      throw new IllegalArgumentException("Bot count can't be greater than 22.");
+    }
     for(int i = 1; i <= botCount; i++) {
       this.bots.add(new Bot("Bot" + i));
     }
   }
 
-  public void startGame() {
+  public void startGame(int botCount) {
+    populateBots(botCount);
     dealCards();
+
+    System.out.println(getCommunityCardsString());
+    System.out.println(this.player.toString());
+    this.bots.forEach(System.out::println);
   }
 
-  public void stopGame() {}
+  public void stopGame() {
+    determineWinner();
+  }
 
   public void dealCards() {
-    deck.stockCards();
+    this.deck.restockCards();
     this.communityCards.clear();
-    this.player.getHand().getCards().clear();
 
     this.communityCards.addAll(deck.dealHand(5));
-    this.player.getHand().getCards().addAll(deck.dealHand(5));
-    this.player.getHand().getCards().addAll(this.communityCards);
 
-    this.player.getHand().getCards().forEach(System.out::println);
+    this.player.getHand().setCards(deck.dealHand(2));
+    for(Bot bot : this.bots) {
+      bot.getHand().setCards(deck.dealHand(2));
+    }
   }
 
   public void determineWinner() {}
+
+  private void preFlopRound() {}
+
+  private void FlopRound() {}
+
+  private void TurnRound() {}
+
+  private void RiverRound() {}
+
+  private void ShowDownRound() {}
 }
