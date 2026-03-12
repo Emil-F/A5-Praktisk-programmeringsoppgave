186ff952daf298bc4b0f25494712c07451947101
Emil-F <e.fagerjord@gmail.com>
2026-03-12 23:47:12 +0100

    feat: added header.css file and created test classes for DeckOfCards and Hand

--- src/test/java/edu/ntnu/idatt2003/emil/a5/model/DeckOfCardsTest.java
+++ src/test/java/edu/ntnu/idatt2003/emil/a5/model/DeckOfCardsTest.java
@@ -1,4 +1,16 @@
+package edu.ntnu.idatt2003.emil.a5.model;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class DeckOfCardsTest {
-  
+
+  @Test
+  void stockCards() {
+  }
+
+  @Test
+  void dealHand() {
+  }
 }
--- src/test/java/edu/ntnu/idatt2003/emil/a5/model/hand/HandTest.java
+++ src/test/java/edu/ntnu/idatt2003/emil/a5/model/hand/HandTest.java
@@ -1,4 +1,24 @@
+package edu.ntnu.idatt2003.emil.a5.model.hand;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class HandTest {
-  
+
+  Hand hand;
+
+  @BeforeEach
+  void setUp() {
+    hand  = new Hand();
+  }
+
+  @Test
+  void calculateCardTotal_ReturnsTheHandsTotalCardValue() {
+  }
+
+  @Test
+  void checkHand() {
+  }
 }
