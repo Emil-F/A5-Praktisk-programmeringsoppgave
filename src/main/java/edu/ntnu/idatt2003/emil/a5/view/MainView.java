f48f3608a8e18552e7a7fb4ca8555310aab59970
Emil-F <e.fagerjord@gmail.com>
2026-03-12 23:36:38 +0100

    feat: added classes related to the App

--- src/main/java/edu/ntnu/idatt2003/emil/a5/view/MainView.java
+++ src/main/java/edu/ntnu/idatt2003/emil/a5/view/MainView.java
@@ -6,10 +6,12 @@ import edu.ntnu.idatt2003.emil.a5.view.sub.Header;
 import javafx.scene.layout.BorderPane;
 
 public class PokerView extends BorderPane {
+  private Header header;
+
   public PokerView(PokerController pokerController) {
     setId("main_view");
     getStylesheets().add(getClass().getResource("/css/MainView.css").toExternalForm());
-    setTop(new Header(this));
+    setTop(new Header(pokerController, this));
     setCenter(new CommunityCards(pokerController));
   }
 }
