2b347b7cce65de6e393a7c7eccb0af2a9bb9ee28
Emil-F <e.fagerjord@gmail.com>
2026-03-15 01:39:27 +0100

    test[Hand]: added tests for checkHand and all rank checking methods in Hand

--- src/test/java/edu/ntnu/idatt2003/emil/a5/model/hand/HandTest.java
+++ src/test/java/edu/ntnu/idatt2003/emil/a5/model/hand/HandTest.java
@@ -4,18 +4,31 @@ import edu.ntnu.idatt2003.emil.a5.model.PlayingCard;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class HandTest {
 
-  Hand hand;
+  private Hand hand;
+  private List<PlayingCard> highCardHand;
+
+  private PlayingCard card(char suit, int face) {
+    return new PlayingCard(suit, face);
+  }
 
   @BeforeEach
   void setUp() {
     hand  = new Hand();
+    highCardHand = List.of(
+        card('S', 2),
+        card('H', 5),
+        card('D', 7),
+        card('C', 9),
+        card('S', 11),
+        card('H', 13),
+        card('D', 3)
+    );
   }
 
   @Test
@@ -23,160 +36,510 @@ class HandTest {
   }
 
   @Test
-  void checkHand() {
+  void checkHand_returnsRoyalFlushIfHandHasRoyalFlush() {
+    hand.setCards(List.of(card('S', 1), card('S', 10)));
+    List<PlayingCard> royalFlush = List.of(
+      card('S', 11),
+      card('S', 12),
+      card('S', 13),
+      card('S', 2),
+      card('S', 3)
+    );
+    int cardTotal = 52;
+
+    HandCheckResult result = new HandCheckResult(cardTotal, HandRank.ROYAL_FLUSH);
+
+    assertEquals(result, hand.checkHand(royalFlush));
+  }
+
+  @Test
+  void checkHand_returnsStraightFlushIfHandHasStraightFlush() {
+    hand.setCards(List.of(card('S', 1), card('S', 3)));
+    List<PlayingCard> straightFlush = List.of(
+      card('S', 4),
+      card('S', 5),
+      card('S', 6),
+      card('S', 7),
+      card('H', 3)
+    );
+    int cardTotal = 29;
+
+    HandCheckResult result = new HandCheckResult(cardTotal, HandRank.STRAIGHT_FLUSH);
+
+    assertEquals(result, hand.checkHand(straightFlush));
+  }
+
+  @Test
+  void checkHand_returnsFourOfAKindIfHandHasFourOfAKind() {
+    hand.setCards(List.of(card('S', 1), card('S', 3)));
+    List<PlayingCard> fourOfAKind = List.of(
+      card('H', 3),
+      card('D', 3),
+      card('H', 6),
+      card('S', 7),
+      card('C', 3)
+    );
+    int cardTotal = 26;
+
+    HandCheckResult result = new HandCheckResult(cardTotal, HandRank.FOUR_OF_A_KIND);
+
+    assertEquals(result, hand.checkHand(fourOfAKind));
+  }
+
+  @Test
+  void checkHand_returnsFullHouseIfHandHasFullHouse() {
+    hand.setCards(List.of(card('S', 1), card('S', 3)));
+    List<PlayingCard> fullHouse = List.of(
+      card('H', 3),
+      card('D', 3),
+      card('H', 6),
+      card('S', 2),
+      card('C', 2)
+    );
+    int cardTotal = 20;
+
+    HandCheckResult result = new HandCheckResult(cardTotal, HandRank.FULL_HOUSE);
+
+    assertEquals(result, hand.checkHand(fullHouse));
+  }
+
+  @Test
+  void checkHand_returnsFlushIfHandHasFlush() {
+    hand.setCards(List.of(card('S', 1), card('H', 4)));
+    List<PlayingCard> flush = List.of(
+      card('H', 6),
+      card('H', 10),
+      card('H', 13),
+      card('S', 8),
+      card('H', 2)
+    );
+    int cardTotal = 44;
+
+    HandCheckResult result = new HandCheckResult(cardTotal, HandRank.FLUSH);
+
+    assertEquals(result, hand.checkHand(flush));
+  }
+
+  @Test
+  void checkHand_returnsStraightIfHandHasStraight() {
+    hand.setCards(List.of(card('S', 1), card('D', 3)));
+    List<PlayingCard> straight = List.of(
+      card('H', 4),
+      card('C', 5),
+      card('H', 6),
+      card('S', 7),
+      card('H', 2)
+    );
+    int cardTotal = 28;
+
+    HandCheckResult result = new HandCheckResult(cardTotal, HandRank.STRAIGHT);
+
+    assertEquals(result, hand.checkHand(straight));
+  }
+
+  @Test
+  void checkHand_returnsThreeOfAKindIfHandHasThreeOfAKind() {
+    hand.setCards(List.of(card('S', 1), card('D', 3)));
+    List<PlayingCard> threeOfAKind = List.of(
+      card('H', 3),
+      card('C', 5),
+      card('H', 6),
+      card('S', 3),
+      card('H', 2)
+    );
+    int cardTotal = 23;
+
+    HandCheckResult result = new HandCheckResult(cardTotal, HandRank.THREE_OF_A_KIND);
+
+    assertEquals(result, hand.checkHand(threeOfAKind));
+  }
+
+  @Test
+  void checkHand_returnsTwoPairsIfHandHasTwoPairs() {
+    hand.setCards(List.of(card('S', 1), card('D', 3)));
+    List<PlayingCard> twoPairs = List.of(
+      card('H', 3),
+      card('C', 5),
+      card('H', 6),
+      card('S', 5),
+      card('H', 2)
+    );
+    int cardTotal = 25;
+
+    HandCheckResult result = new HandCheckResult(cardTotal, HandRank.TWO_PAIRS);
+
+    assertEquals(result, hand.checkHand(twoPairs));
+  }
+
+  @Test
+  void checkHand_returnsOnePairIfHandHasOnePair() {
+    hand.setCards(List.of(card('S', 1), card('D', 3)));
+    List<PlayingCard> onePair = List.of(
+      card('H', 3),
+      card('C', 5),
+      card('H', 6),
+      card('S', 7),
+      card('H', 2)
+    );
+    int cardTotal = 27;
+
+    HandCheckResult result = new HandCheckResult(cardTotal, HandRank.ONE_PAIR);
+
+    assertEquals(result, hand.checkHand(onePair));
+  }
+
+  @Test
+  void checkHand_returnsHighCardIfHandHasHighCard() {
+    hand.setCards(List.of(card('S', 2), card('H', 5)));
+    List<PlayingCard> highCard = List.of(
+      card('D', 7),
+      card('C', 9),
+      card('S', 11),
+      card('H', 13),
+      card('D', 3)
+    );
+    int cardTotal = 50;
+
+    HandCheckResult result = new HandCheckResult(cardTotal, HandRank.HIGH_CARD);
+
+    assertEquals(result, hand.checkHand(highCard));
   }
 
   @Test
   void hasRoyalFlush_returnsTrueIfHandHasRoyalFlush() {
-    List<PlayingCard> royalFlushHand = new ArrayList<>();
+    List<PlayingCard> royalFlush = List.of(
+      card('S', 1),
+      card('S', 10),
+      card('S', 11),
+      card('S', 12),
+      card('S', 13),
+      card('S', 2),
+      card('S', 3)
+    );
 
-    royalFlushHand.add(new PlayingCard('S', 1));
-    royalFlushHand.add(new PlayingCard('S', 10));
-    royalFlushHand.add(new PlayingCard('S', 11));
-    royalFlushHand.add(new PlayingCard('S', 12));
-    royalFlushHand.add(new PlayingCard('S', 13));
-    royalFlushHand.add(new PlayingCard('S', 2));
-    royalFlushHand.add(new PlayingCard('S', 3));
-
-    assertTrue(hand.hasRoyalFlush(royalFlushHand));
+    assertTrue(hand.hasRoyalFlush(royalFlush));
   }
 
   @Test
   void hasRoyalFlush_returnsFalseIfHandDoesNotHaveRoyalFlush() {
-    List<PlayingCard> cards = new ArrayList<>();
-
-    cards.add(new PlayingCard('S', 1));
-    cards.add(new PlayingCard('S', 5));
-    cards.add(new PlayingCard('S', 11));
-    cards.add(new PlayingCard('S', 12));
-    cards.add(new PlayingCard('S', 13));
-    cards.add(new PlayingCard('D', 2));
-    cards.add(new PlayingCard('C', 13));
-
-    assertFalse(hand.hasRoyalFlush(cards));
+    assertFalse(hand.hasRoyalFlush(highCardHand));
   }
 
   @Test
   void hasRoyalFlush_returnsFalseIfCardsDontAllHaveTheSameSuit() {
-    List<PlayingCard> cards = new ArrayList<>();
-
-    cards.add(new PlayingCard('S', 1));
-    cards.add(new PlayingCard('S', 10));
-    cards.add(new PlayingCard('H', 11));
-    cards.add(new PlayingCard('S', 12));
-    cards.add(new PlayingCard('S', 13));
-    cards.add(new PlayingCard('H', 13));
-    cards.add(new PlayingCard('C', 13));
+    List<PlayingCard> cards = List.of(
+      card('S', 1),
+      card('S', 10),
+      card('H', 11),
+      card('S', 12),
+      card('S', 13),
+      card('H', 13),
+      card('C', 13)
+    );
 
     assertFalse(hand.hasRoyalFlush(cards));
   }
 
   @Test
   void hasStraightFlush_returnsTrueIfHandHasStraightFlush() {
-    List<PlayingCard> straightFlush = new ArrayList<>();
-
-    straightFlush.add(new PlayingCard('S', 7));
-    straightFlush.add(new PlayingCard('S', 8));
-    straightFlush.add(new PlayingCard('S', 9));
-    straightFlush.add(new PlayingCard('S', 10));
-    straightFlush.add(new PlayingCard('S', 11));
-    straightFlush.add(new PlayingCard('S', 1));
-    straightFlush.add(new PlayingCard('S', 2));
+    List<PlayingCard> straightFlush = List.of(
+      card('S', 7),
+      card('S', 8),
+      card('S', 9),
+      card('S', 10),
+      card('S', 11),
+      card('S', 1),
+      card('S', 2)
+    );
 
     assertTrue(hand.hasStraightFlush(straightFlush));
   }
 
   @Test
   void hasStraightFlush_returnsFalseIfHandDoesNotHaveStraightFlush() {
-    List<PlayingCard> cards = new ArrayList<>();
-
-    cards.add(new PlayingCard('S', 7));
-    cards.add(new PlayingCard('S', 2));
-    cards.add(new PlayingCard('S', 9));
-    cards.add(new PlayingCard('S', 10));
-    cards.add(new PlayingCard('S', 11));
-    cards.add(new PlayingCard('S', 5));
-    cards.add(new PlayingCard('S', 4));
-
-    assertFalse(hand.hasStraightFlush(cards));
+    assertFalse(hand.hasStraightFlush(highCardHand));
   }
 
   @Test
   void hasStraightFlush_returnsFalseIfCardsDontAllHaveTheSameSuit() {
-    List<PlayingCard> cards = new ArrayList<>();
-
-    cards.add(new PlayingCard('S', 7));
-    cards.add(new PlayingCard('S', 8));
-    cards.add(new PlayingCard('H', 9));
-    cards.add(new PlayingCard('S', 10));
-    cards.add(new PlayingCard('S', 11));
-    cards.add(new PlayingCard('S', 2));
-    cards.add(new PlayingCard('S', 1));
+    List<PlayingCard> cards = List.of(
+      card('D', 7),
+      card('S', 8),
+      card('H', 9),
+      card('S', 10),
+      card('S', 11),
+      card('S', 2),
+      card('S', 1)
+    );
 
     assertFalse(hand.hasStraightFlush(cards));
   }
 
   @Test
   void hasFourOfAKind_returnsTrueIfHandHasFourOfAKind() {
-    List<PlayingCard> fourOfAKind = new ArrayList<>();
-
-    fourOfAKind.add(new PlayingCard('S', 5));
-    fourOfAKind.add(new PlayingCard('H', 5));
-    fourOfAKind.add(new PlayingCard('D', 5));
-    fourOfAKind.add(new PlayingCard('C', 5));
-    fourOfAKind.add(new PlayingCard('C', 10));
-    fourOfAKind.add(new PlayingCard('S', 11));
-    fourOfAKind.add(new PlayingCard('S', 11));
+    List<PlayingCard> fourOfAKind = List.of(
+      card('S', 5),
+      card('H', 5),
+      card('D', 5),
+      card('C', 5),
+      card('C', 10),
+      card('S', 11),
+      card('H', 11)
+    );
 
     assertTrue(hand.hasFourOfAKind(fourOfAKind));
   }
 
   @Test
   void hasFourOfAKind_returnsFalseIfHandDoesNotHaveFourOfAKind() {
-    List<PlayingCard> fourOfAKind = new ArrayList<>();
+    List<PlayingCard> threeOfAKind = List.of(
+      card('S', 5),
+      card('H', 5),
+      card('D', 5),
+      card('C', 2),
+      card('C', 10),
+      card('S', 11),
+      card('H', 11)
+    );
 
-    fourOfAKind.add(new PlayingCard('S', 5));
-    fourOfAKind.add(new PlayingCard('H', 5));
-    fourOfAKind.add(new PlayingCard('D', 6));
-    fourOfAKind.add(new PlayingCard('C', 5));
-    fourOfAKind.add(new PlayingCard('C', 10));
-    fourOfAKind.add(new PlayingCard('S', 11));
-    fourOfAKind.add(new PlayingCard('S', 11));
-
-    assertFalse(hand.hasFourOfAKind(fourOfAKind));
+    assertAll(
+      () -> assertFalse(hand.hasFourOfAKind(highCardHand)),
+      () -> assertFalse(hand.hasFourOfAKind(threeOfAKind))
+    );
   }
 
   @Test
-  void hasFullHouse_returnsTrueIfHandHasFullHouse() {}
+  void hasFullHouse_returnsTrueIfHandHasFullHouse() {
+    List<PlayingCard> fullHouseV1 = List.of(
+      card('S', 6),
+      card('H', 6),
+      card('D', 6),
+      card('C', 12),
+      card('H', 12),
+      card('S', 1),
+      card('H', 2)
+    );
 
+    List<PlayingCard> fullHouseV2 = List.of(
+      card('S', 6),
+      card('H', 5),
+      card('D', 5),
+      card('C', 5),
+      card('H', 12),
+      card('S', 1),
+      card('H', 6)
+    );
+
+    assertAll(
+      () -> assertTrue(hand.hasFullHouse(fullHouseV1)),
+      () -> assertTrue(hand.hasFullHouse(fullHouseV2))
+    );
+  }
+
+  @Test
+  void hasFullHouse_returnsFalseIfHandDoesNotHaveFullHouse() {
+    assertFalse(hand.hasFullHouse(highCardHand));
+  }
+
+  @Test
+  void hasFlush_returnsTrueIfHandHasFlush() {
+    List<PlayingCard> flushV1 = List.of(
+        card('S', 1),
+        card('S', 3),
+        card('S', 4),
+        card('S', 6),
+        card('H', 8),
+        card('S', 10),
+        card('H', 11)
+    );
+
+    List<PlayingCard> flushV2= List.of(
+        card('H', 1),
+        card('H', 3),
+        card('H', 4),
+        card('H', 6),
+        card('H', 8),
+        card('H', 10),
+        card('H', 11)
+    );
+
+    assertAll(
+        () -> assertTrue(hand.hasFlush(flushV1)),
+        () -> assertTrue(hand.hasFlush(flushV2))
+    );
+  }
+
+  @Test
+  void hasFlush_returnsFalseIfHandDoesNotHaveFlush() {
+    assertFalse(hand.hasFlush(highCardHand));
+  }
+
+  @Test
+  void hasStraight_returnsTrueIfHandHasStraight() {
+    List<PlayingCard> straightV1 = List.of(
+        card('S', 3),
+        card('H', 1),
+        card('D', 6),
+        card('C', 7),
+        card('C', 8),
+        card('S', 9),
+        card('S', 10)
+    );
+
+    List<PlayingCard> straightV2 = List.of(
+        card('S', 13),
+        card('H', 3),
+        card('D', 4),
+        card('C', 5),
+        card('C', 6),
+        card('S', 7),
+        card('S', 10)
+    );
+
+    List<PlayingCard> straightV3 = List.of(
+        card('S', 13),
+        card('H', 4),
+        card('D', 5),
+        card('C', 6),
+        card('C', 7),
+        card('S', 8),
+        card('S', 1)
+    );
+
+    List<PlayingCard> straightV4 = List.of(
+        card('S', 13),
+        card('H', 4),
+        card('D', 11),
+        card('C', 7),
+        card('C', 8),
+        card('S', 9),
+        card('S', 10)
+    );
+
+    List<PlayingCard> straightV5 = List.of(
+        card('S', 7),
+        card('S', 8),
+        card('S', 9),
+        card('S', 10),
+        card('S', 11),
+        card('S', 1),
+        card('S', 2)
+    );
+
+    assertAll(
+        () -> assertTrue(hand.hasStraight(straightV1)),
+        () -> assertTrue(hand.hasStraight(straightV2)),
+        () -> assertTrue(hand.hasStraight(straightV3)),
+        () -> assertTrue(hand.hasStraight(straightV4)),
+        () -> assertTrue(hand.hasStraight(straightV5))
+    );
+  }
+
+  @Test
+  void hasStraight_returnsFalseIfHandDoesNotHaveStraight() {
+    assertFalse(hand.hasStraight(highCardHand));
+  }
 
   @Test
   void hasThreeOfAKind_returnsTrueIfHandHasThreeOfAKind() {
-    List<PlayingCard> threeOfAKind = new ArrayList<>();
-
-    threeOfAKind.add(new PlayingCard('S', 5));
-    threeOfAKind.add(new PlayingCard('H', 5));
-    threeOfAKind.add(new PlayingCard('D', 5));
-    threeOfAKind.add(new PlayingCard('C', 9));
-    threeOfAKind.add(new PlayingCard('C', 10));
-    threeOfAKind.add(new PlayingCard('S', 11));
-    threeOfAKind.add(new PlayingCard('S', 11));
+    List<PlayingCard> threeOfAKind = List.of(
+      card('S', 5),
+      card('H', 5),
+      card('D', 5),
+      card('C', 9),
+      card('C', 10),
+      card('S', 11),
+      card('S', 11)
+    );
 
     assertTrue(hand.hasThreeOfAKind(threeOfAKind));
   }
 
   @Test
   void hasThreeOfAKind_returnsFalseIfHandDoesNotHaveThreeOfAKind() {
-    List<PlayingCard> threeOfAKind = new ArrayList<>();
+    List<PlayingCard> fourOfAKind = List.of(
+        card('S', 5),
+        card('H', 5),
+        card('D', 5),
+        card('C', 5),
+        card('C', 10),
+        card('S', 11),
+        card('S', 11)
+    );
 
-    threeOfAKind.add(new PlayingCard('S', 5));
-    threeOfAKind.add(new PlayingCard('H', 5));
-    threeOfAKind.add(new PlayingCard('D', 6));
-    threeOfAKind.add(new PlayingCard('C', 7));
-    threeOfAKind.add(new PlayingCard('C', 10));
-    threeOfAKind.add(new PlayingCard('S', 11));
-    threeOfAKind.add(new PlayingCard('S', 11));
+    assertAll(
+        () -> assertFalse(hand.hasThreeOfAKind(highCardHand)),
+        () -> assertFalse(hand.hasThreeOfAKind(fourOfAKind))
+    );
+  }
 
-    assertFalse(hand.hasThreeOfAKind(threeOfAKind));
+  @Test
+  void hasTwoPairs_returnsTrueIfHandHasTwoPairs() {
+    List<PlayingCard> twoPairs = List.of(
+        card('S', 5),
+        card('H', 5),
+        card('D', 1),
+        card('C', 2),
+        card('C', 10),
+        card('S', 10),
+        card('S', 12)
+    );
+
+    assertTrue(hand.hasTwoPairs(twoPairs));
+  }
+
+  @Test
+  void hasTwoPairs_returnsFalseIfHandHasOnePair() {
+    List<PlayingCard> onePair = List.of(
+        card('S', 5),
+        card('H', 5),
+        card('D', 1),
+        card('C', 2),
+        card('C', 10),
+        card('S', 11),
+        card('S', 12)
+    );
+
+    assertFalse(hand.hasTwoPairs(onePair));
+  }
+
+  @Test
+  void hasTwoPairs_returnsFalseIfHandDoesNotHaveTwoPairs() {
+    assertFalse(hand.hasTwoPairs(highCardHand));
+  }
+
+  @Test
+  void hasOnePair_returnsTrueIfHandHasOnePair() {
+    List<PlayingCard> onePair = List.of(
+        card('S', 5),
+        card('H', 5),
+        card('D', 1),
+        card('C', 2),
+        card('C', 10),
+        card('S', 11),
+        card('S', 12)
+    );
+
+    assertTrue(hand.hasOnePair(onePair));
+  }
+
+  @Test
+  void hasOnePair_returnsFalseIfHandHasTwoPairs() {
+    List<PlayingCard> twoPairs = List.of(
+        card('S', 5),
+        card('H', 5),
+        card('D', 1),
+        card('C', 2),
+        card('C', 10),
+        card('S', 10),
+        card('S', 12)
+    );
+
+    assertFalse(hand.hasOnePair(twoPairs));
+  }
+
+  @Test
+  void hasOnePair_returnsFalseIfHandDoesNotHaveOnePair() {
+    assertFalse(hand.hasOnePair(highCardHand));
   }
 }
