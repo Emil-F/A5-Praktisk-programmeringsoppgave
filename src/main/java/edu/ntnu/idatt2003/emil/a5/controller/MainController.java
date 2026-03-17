26c50dc913412165cd54bf3218bd717b75df356d
Emil-F <e.fagerjord@gmail.com>
2026-03-17 01:19:01 +0100

    feat: created PokerService to make it easier for data to flow to the PokerController

--- src/main/java/edu/ntnu/idatt2003/emil/a5/controller/MainController.java
+++ src/main/java/edu/ntnu/idatt2003/emil/a5/controller/MainController.java
@@ -1,17 +1,20 @@
 package edu.ntnu.idatt2003.emil.a5.controller;
 
-import edu.ntnu.idatt2003.emil.a5.model.poker.PokerGame;
+import edu.ntnu.idatt2003.emil.a5.model.PokerService;
+import edu.ntnu.idatt2003.emil.a5.model.PokerGame;
 import edu.ntnu.idatt2003.emil.a5.view.MainView;
 import javafx.scene.layout.BorderPane;
 
 public class MainController {
   private final MainView mainView;
   private final PokerGame game;
+  private final PokerService pokerService;
   private final PokerController pokerController;
 
   public MainController() {
     this.game = new PokerGame();
-    this.pokerController = new PokerController(this, game);
+    this.pokerService = new PokerService(game);
+    this.pokerController = new PokerController(this, pokerService);
     this.mainView = new MainView(pokerController);
   }
 
--- src/main/java/edu/ntnu/idatt2003/emil/a5/model/PokerService.java
+++ src/main/java/edu/ntnu/idatt2003/emil/a5/model/PokerService.java
@@ -1,4 +1,91 @@
 package edu.ntnu.idatt2003.emil.a5.model;
 
+import edu.ntnu.idatt2003.emil.a5.model.poker.PlayingCard;
+import edu.ntnu.idatt2003.emil.a5.model.poker.hand.HandCheckResult;
+import edu.ntnu.idatt2003.emil.a5.model.poker.round.PlayerAction;
+import edu.ntnu.idatt2003.emil.a5.model.poker.round.PokerRound;
+import edu.ntnu.idatt2003.emil.a5.model.poker.round.Pot;
+import edu.ntnu.idatt2003.emil.a5.model.poker.round.Round;
+import edu.ntnu.idatt2003.emil.a5.model.users.Bot;
+import edu.ntnu.idatt2003.emil.a5.model.users.Participant;
+import edu.ntnu.idatt2003.emil.a5.model.users.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PokerService {
+
+  private final PokerGame game;
+
+  public PokerService(PokerGame game) {
+    this.game = game;
+  }
+
+  public Player getPlayer() {
+    return game.getPlayer();
+  }
+
+  public List<Bot> getBots() {
+    return game.getBots();
+  }
+
+  public List<Participant> getParticipants() {
+    return game.getParticipants();
+  }
+
+  public List<PlayingCard> getCommunityCards() {
+    return game.getCurrentRound().getCommunityCards();
+  }
+
+  public Pot getPot() {
+    return game.getCurrentRound().getPot();
+  }
+
+  public PokerRound getCurrentRound() {
+    return game.getCurrentRound();
+  }
+
+  public Round getCurrentRoundState() {
+    return game.getCurrentRound().getRoundState().getCurrentState();
+  }
+
+  public HandCheckResult getPlayerHandCheckResult() {
+    return game.getCurrentRound().getHandChecker().checkHand(getPlayer(), getCommunityCards());
+  }
+
+  public HandCheckResult getLastWinnerResult() {
+    return game.getCurrentRound().getLastWinner();
+  }
+
+  public String getCardsOfHearts() {
+    List<PlayingCard> cards = new ArrayList<>(getPlayer().getCards());
+    cards.addAll(getCommunityCards());
+
+    List<PlayingCard> filtered = cards.stream().filter(c -> c.getSuit() == 'H').toList();
+
+    StringBuilder cardsString = new StringBuilder();
+    for (PlayingCard card : filtered) {
+      cardsString.append(card.getAsString()).append(" ");
+    }
+    return cardsString.toString();
+  }
+
+  public String hasQueenOfSpades() {
+    List<PlayingCard> cards = new ArrayList<>(getPlayer().getCards());
+    cards.addAll(getCommunityCards());
+
+    return cards.stream().anyMatch(c -> c.getSuit() == 'S' && c.getFace() == 12) ? "Yes" : "No";
+  }
+
+  public void advanceRound() {
+    game.advanceRound();
+  }
+
+  public void startGame() {
+    game.startGame();
+  }
+
+  public void handleAction(Participant participant, PlayerAction action) {
+    game.getCurrentRound().getBettingRound().handleAction(participant, action);
+  }
 }
